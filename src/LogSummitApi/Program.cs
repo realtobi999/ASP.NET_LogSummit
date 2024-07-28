48843b8e00fb028f05099416967b057ae3e5826b
realtobi999 <tobiasfilgas@gmail.com>
2024-07-28 20:01:38 +0200

    [layout]

---
+++ src/LogSummitApi/Program.cs
@@ -0,0 +1,26 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        {
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+        }
+
+        var app = builder.Build();
+        {
+            if (app.Environment.IsDevelopment())
+            {   
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.MapControllers();
+            
+            app.Run();
+        }
+    }
+}
