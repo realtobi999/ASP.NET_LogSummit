a23f5080f9d8e2929256b2829918932bec5d3794
realtobi999 <tobiasfilgas@gmail.com>
2024-08-09 00:01:03 +0200

    [testing] integration test for route update endpoint

--- backend/LogSummitApi.Tests/Integration/Endpoints/RouteTests.cs
+++ backend/LogSummitApi.Tests/Integration/Endpoints/RouteTests.cs
@@ -150,4 +150,52 @@ public class RouteTests
 
         content.Id.Should().Be(route.Id);
     }
+
+    [Fact]
+    public async void Update_Returns204AndIsUpdated()
+    {
+        // prepare
+        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var user = new User().WithFakeData();
+        var summit = new Summit().WithFakeData(user);
+        var route = new Route().WithFakeData(user, summit);
+
+        var jwt = JwtTestUtils.CreateInstance().Generate([
+            new Claim(ClaimTypes.Role, "User"),
+            new Claim("UserId", user.Id.ToString()),
+        ]);
+        client.DefaultRequestHeaders.Add("Authorization", $"BEARER {jwt}");
+
+        var create1 = await client.PostAsJsonAsync("v1/api/auth/register", user.ToRegisterUserDto());
+        create1.StatusCode.Should().Be(HttpStatusCode.Created);
+        var create2 = await client.PostAsJsonAsync("v1/api/summit", summit.ToCreateSummitDto());
+        create2.StatusCode.Should().Be(HttpStatusCode.Created);
+        var create3 = await client.PostAsJsonAsync("v1/api/summit/route", route.ToCreateRouteDto());
+        create3.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        // act & assert
+        var updateDto = new UpdateRouteDto
+        {
+            UserId = route.UserId,
+            SummitId = route.SummitId,
+            Name = "test",
+            Description = "test",
+            Distance = route.Distance,
+            ElevationGain = route.ElevationGain,
+            ElevationLoss = route.ElevationLoss,
+            Coordinates = route.Coordinates,
+        };
+
+        var response = await client.PutAsJsonAsync($"v1/api/summit/route/{route.Id}", updateDto);
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+        var get = await client.GetAsync($"v1/api/summit/route/{route.Id}");
+        get.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var content = await get.Content.ReadFromJsonAsync<RouteDto>() ?? throw new NullReferenceException();
+
+        content.Id.Should().Be(route.Id);
+        content.Name.Should().Be(updateDto.Name);
+        content.Description.Should().Be(updateDto.Description);
+    }
 }
